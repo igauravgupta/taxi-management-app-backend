d989885d4b9da9da6742d84e77389336d110bc95
Gaurav Gupta <itisgauravgupta@gmail.com>
2024-12-02 22:59:13 +0530

    initial-models

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
 
 const app = express();
 
 dotenv.config();
 
 app.use(cors());
+
+connectDB()
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 export default app;
---
+++ src/models/user.model.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    firstName: {
+      type: String,
+      required: true,
+      min: [3, "First name must be at least 3 characters long"],
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      min: [3, "Last name must be at least 3 characters long"],
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false, // will not return password in response
+    },
+    socketId: {
+      type: String,
+    },
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+userSchema.methods.generateToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
